5027911d60312564ac434ddc020ea4a94a41d402
MarcosCruzRascon <73098195+MarcosCruzRascon@users.noreply.github.com>
2021-02-02 00:08:22 +0100

    Whatsapp JSONF Fijo

--- whatsapp/js/libreria.js
+++ whatsapp/js/libreria.js
@@ -1,73 +1,47 @@
 $(document).ready(function() {
-    $("thead").eq(0).css({
-        "color": "white",
-        "font-size": "1.5,",
-        "background-color": "gray"
-    });
+    $.getJSON("./json/identificado.json",
+        function(data, textStatus, jqXHR) {
+            if (textStatus = "success") {
+                var misDatos = data;
+                for (let i = 0; i < misDatos.contactos.length; i++) {
+                    var div = $("<div>");
+                    $(div).text(misDatos.contactos[i].nombre);
+                    $(div).data("numero", misDatos.contactos[i].telefono);
 
-    $("tbody tr:odd").css({ "background-color": "lightgray" })
-    $("tbody td").click(function() {
+                    $(div).click(function() {
+                        var este = $(this).data("numero")
+                        borrar();
+                        $.get("./json/conversacion.json", function(data, textStatus, jqXHR) {
+                            var conversacion = data;
+                            for (let i = 0; i < conversacion.length; i++) {
+                                if (conversacion[i].sender == este || conversacion[i].receiver == este) {
+                                    var divmensaje = $("<div>");
+                                    var texto = $("<p>").text(conversacion[i].mensaje);
+                                    var fechaHora = $("<small>").text(conversacion[i].fechaHora);
+                                    $(divmensaje).append(texto);
+                                    $(divmensaje).append(fechaHora);
+                                    if (misDatos.mitelefono == conversacion[i].sender) {
+                                        $(divmensaje).css({ "background-color": "green" });
+                                        $(divmensaje).addClass("enviados");
+                                    } else {
+                                        $(divmensaje).css({ "background-color": "grey" });
+                                        $(divmensaje).addClass("recibidos");
+                                    }
+                                    $("#chat").append($(divmensaje));
+                                }
+                            }
 
-        /* if ($(this).css("color") === "rgb(0, 0, 0)") {
-             $(this).css("color", "red")
-         } else {
-             $(this).css("color", "black")
-         }*/
-        /*if ($(this).text() != "") {
-            var input = $("<input>").attr("type", "text").val($(this).text());
-            $(this).empty().append(input);
-            input.focus();
-            input.blur(function() {
-                $(this).parent().html($(this).val());
-            })
-        }*/
-    })
+                        });
+                    });
+                    $("#conversaciones").append($(div));
+                }
+            }
 
-
-
-    $("tbody tr").dblclick(function() {
-        $(this).hide();
-    })
-    $("thead").dblclick(function() {
-        $(this).parent().find("tbody tr").show();
-    })
-
-    $("tbody tr").click(function() {
-        $(this).children().css("background-color", "blue");
-        $(this).prevAll().children().css("background-color", "red");
-        $(this).nextAll().children().css("background-color", "green")
-    })
-
-    $("button").click(function() {
-        var div = $("<div>");
-        var span = $("<span>");
-        $(div).append(span);
-        $(this).parent().append(div);
-        $(span).text("X");
-
-        $("div").css({
-            "z-index": 999,
-            "position": "fixed",
-            "background-color": "black",
-            "opacity": 0.8,
-            "height": "100%",
-            "width": "100%",
-            "left": 0,
-            "top": 0
-        }).appendTo($(this).parent());
-
-
-        $("span").css({
-            "color": "white",
-            "font-size": "200%",
-            "position": "fixed",
-            "right": 20,
-            "top": 0
-        })
-
-        $("span").click(function() {
-            $("span").parent().remove();
-        })
-    })
+        }
+    );
 
 })
+
+function borrar() {
+    $("#chat").children().remove();
+}
